3d6d5f8f0e6e06cef18d0f2e77f85e5da3624d13
p-le <p-le@dimage.co.jp>
2017-11-02 16:30:47 +0900

    test

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h4>Testing Changed</h4>
+        <h4>Testing Changed Polling</h4>
         <header>
           <label>{ this.state.count }</label>
           <button onClick={this.handleClick}>Click Me!</button>
